ab006280cd2e2264a932fcabdd134dcb5adfb1ff
Kidami13 <ulyanaboo1407@gmail.com>
2020-11-07 15:36:56 +0200

    HW_8

---
+++ HW_8.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+	int ulyamass[7][7];
+	int ulyaMaxVal = 0;
+	int range = 100;
+	int ulyaMinVal = range;
+	
+
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 7; j++) {
+			ulyamass[i][j] = rand() % range;
+		}
+	}	
+
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 7; j++) {
+			if (ulyamass[i][j] > ulyaMaxVal) {
+				ulyaMaxVal = ulyamass[i][j];
+				
+			}
+			if (ulyamass[i][j] < ulyaMinVal) {
+				ulyaMinVal = ulyamass[i][j];
+			}
+		}
+	}
+
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 7; j++) {
+			cout << ulyamass[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << ulyaMaxVal << " Max Value" << endl;
+	cout << ulyaMinVal << " Min Value" << endl;
+}
+
